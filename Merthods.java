56ef632358a3e0cf87f0c4251a33c97fbfd8657e
LTeixeira4909 <ipadljteixeira@gmail.com>
2023-09-14 10:34:15 -0400

    Methods

---
+++ Merthods.java
@@ -0,0 +1,32 @@
+public class Merthods {
+    public static void main(String [] args){
+        method1(4);
+        method1(7);
+    }
+    public static void method1(int x){
+        int y = x+1;
+        System.out.println("added one to get " + y);
+    }
+    public static int method2(String name, int x){
+        System.out.println("Hello" + name);
+        int rem = x%2;
+        return rem;
+    }
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
