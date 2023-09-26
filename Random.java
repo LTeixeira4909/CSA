73b9c7109181f2f52ac37e43cd8f3c4aa22d5636
LTeixeira4909 <ipadljteixeira@gmail.com>
2023-09-26 10:08:37 -0400

    random

---
+++ Random.java
@@ -0,0 +1,11 @@
+public class Random {
+   public static void main(String[] args){
+    double x = Math.random();
+    System.out.println(x);
+ 
+    x = Math.random()*20;
+    System.out.println(x);
+    int a = (int)(Math.random()*51)+50;
+    System.out.println(a);
+} 
+}
