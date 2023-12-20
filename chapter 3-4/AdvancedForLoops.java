e7e5e7708b147c76ce9a1a9e09e95e498394f78b
LTeixeira4909 <ipadljteixeira@gmail.com>
2023-12-20 09:36:09 -0500

    isosceles done

--- chapter 3-4/AdvancedForLoops.java
+++ chapter 3-4/AdvancedForLoops.java
@@ -1,21 +1,67 @@
 public class AdvancedForLoops {
-
-    public void eights() {
-        for (int j = 8; j >= 2; j -= 2) {
-            for (int i = 1; i <= j; i++) {
+    public static void eights() {
+        for (int i = 8; i > 0; i -= 2) {
+            for (int k = 0; k < 8 - i; k += 2) {
                 System.out.print(" ");
             }
-            for (int k = 1; k <= j; k++) {
-                System.out.print(j);
+            for (int j = 0; j < i; j++) {
+                System.out.print(i);
             }
             System.out.println();
         }
+    }
 
+    public static void alternate(int num) {
+        for (int i = 1; i <= num; i++) {
+            for (int j = 1; i % 2 != 0 && j <= i; j++) {
+                System.out.print(j);
+            }
+            for (int k = i; i % 2 == 0 && k > 0; k--) {
+                System.out.print(k);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void isosceles(int num) {
+        for (int i = 1; i <= num; i++) {
+            for (int c = 0; c < num - i; c++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            for (int k = i - 1; k > 0; k--) {
+                System.out.print(k);
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
         AdvancedForLoops a = new AdvancedForLoops();
+        System.out.println();
+        System.out.println("eights");
+        System.out.println();
         a.eights();
 
+        System.out.println();
+
+        System.out.println("alternate");
+        System.out.println();
+        a.alternate(5);
+        System.out.println();
+        a.alternate(6);
+
+        System.out.println();
+
+        System.out.println("isosceles");
+        System.out.println();
+        a.isosceles(5);
+
+        System.out.println();
+
+        System.out.println("upsidedown");
+        System.out.println();
     }
 }
